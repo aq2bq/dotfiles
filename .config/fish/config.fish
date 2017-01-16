e6d4a66d6e77aa20ca11615fef32c9fce162b026
aq2bq <pememo@gmail.com>
2017-01-16 11:41:02 +0900

    Add a nodebrew path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ alias flushdns="dscacheutil -flushcache"
 
 set -x LESS '-g -i -M -R -S -W -z-4 -x4'
 set -x PGDATA /usr/local/var/postgres
+set -x PATH $HOME/.nodebrew/current/bin $PATH
 
 function do_enter
   set -l query (commandline)
