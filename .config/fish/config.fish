44a76282c21d58a76578afadd41084721bbf2cc3
aq2bq <pememo@gmail.com>
2017-05-15 14:33:38 +0900

    Install git-foresta

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,9 @@ alias be="bundle exec"
 alias flushdns="dscacheutil -flushcache"
 alias terminal-notifier="reattach-to-user-namespace terminal-notifier"
 
+# https://github.com/takaaki-kasai/git-foresta
+alias gf="~/bin/git-foresta | less -RSX"
+
 set -x LESS '-g -i -M -R -S -W -z-4 -x4'
 set -x PGDATA /usr/local/var/postgres
 set -x PATH $HOME/.nodebrew/current/bin $HOME/.cargo/bin /usr/local/heroku/bin $PATH
