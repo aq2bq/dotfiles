5c5910838737126eab0a36096fc2fa6a1a5c4119
aq2bq <pememo@gmail.com>
2018-12-18 20:35:09 +0900

    Update a path to Julia 1.0

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 alias emacs="env TERM=xterm-256color /usr/local/Cellar/emacs/26.1_1/bin/emacs -nw"
+alias julia="/Applications/Julia-1.0.app/Contents/Resources/julia/bin/julia"
 # alias postgres_start="pg_ctl -l /usr/local/var/postgres/server.log start"
 # alias postgres_stop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
 # => use `brew services start postgresql` instead of there
