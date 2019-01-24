92b34a0d5b320703b4d106ab2ad7e42892453c26
aq2bq <pememo@gmail.com>
2019-01-24 10:53:51 +0900

    Set env HOMEBREW_INSTALL_CLEANUP
    https://brew.sh/2019/01/09/homebrew-1.9.0/
    
    https://github.com/Homebrew/brew/pull/5472

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,6 +22,7 @@ set GOPATH (go env GOPATH)
 set -x LESS '-g -i -M -R -S -W -z-4 -x4'
 set -x PGDATA /usr/local/var/postgress
 set -x PATH $HOME/.nodebrew/current/bin $HOME/.cargo/bin $HOME/.rbenv/shims $GOPATH/bin $PATH
+set -x HOMEBREW_INSTALL_CLEANUP 1
 
 bind \cj on_enter # Ctrl+j
 bind \cr peco_select_history # Bind for peco history to Ctrl+r
