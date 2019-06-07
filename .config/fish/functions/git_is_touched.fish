5a6c7d509a16980e90e97da2cb43ee4bbe46f611
aq2bq <pememo@gmail.com>
2019-06-07 15:36:46 +0900

    fisher

--- .config/fish/functions/git_is_touched.fish
+++ .config/fish/functions/git_is_touched.fish
@@ -1,12 +1,3 @@
 function git_is_touched -d "Test if there are any changes in the working tree"
-    git_is_repo; and command git status --porcelain 2>/dev/null | command awk '
-        // {
-            z++
-            exit 0
-        }
-
-        END {
-            exit !z
-        }
-    '
+    git_is_staged; or git_is_dirty
 end
