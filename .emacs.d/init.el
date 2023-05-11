8f37b9b6d7b40c4608e3cf8ff0f833aaf38a4764
aq2bq <pememo@gmail.com>
2023-05-11 10:45:34 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,7 +16,7 @@
 (global-set-key (kbd "C-c /") 'comment-or-uncomment-region)
 (global-set-key (kbd "C-c n") 'next-buffer)
 (global-set-key (kbd "C-c b") 'previous-buffer)
-(global-set-key (kbd "C-x l") 'goto-line)
+;; (global-set-key (kbd "C-x l") 'goto-line)
 (global-set-key (kbd "C--") 'undo)
 (global-set-key (kbd "C-x f") 'project-find-file)
 (defvar mac-command-modifier 'super)
@@ -65,7 +65,6 @@
 
 ;; copy mouse drag region
 (setq mouse-drag-copy-region t)
-
 
 ;; for support `ls --dired`
 (let ((gls (substring (shell-command-to-string "which gls") 0 -1)))
@@ -128,6 +127,18 @@
 ;; Emacs global extentions
 ;;;
 
+(leaf good-scroll
+  :doc "Attempt at good pixel-based smooth scrolling in Emacs"
+  :ensure t
+  :bind (("C-v" . good-scroll-up-full-screen)
+         ("M-v" . good-scroll-down-full-screen))
+  :init (good-scroll-mode t))
+
+(leaf topsy
+  :doc "simple alternative to `semantic-stickyfunc-mode`"
+  :ensure t
+  :hook (prog-mode-hook . topsy-mode))
+
 (leaf cus-edit
   :doc "tools for customizing Emacs and Lisp packages"
   :tag "builtin" "faces" "help"
@@ -137,7 +148,8 @@
   :ensure t
   :config
   ;; (load-theme 'doom-laserwave t nil)
-  (load-theme 'doom-dracula t nil)
+  ;; (load-theme 'doom-dracula t nil)
+  (load-theme 'doom-challenger-deep t nil)
   (doom-themes-neotree-config)
   :custom
   (doom-themes-visual-bell-config . t)
@@ -145,7 +157,13 @@
   (doom-dracula-comment-bg . t)
   (doom-modeline-bar . t)
   (doom-themes-enable-italic . t)
-  (doom-themes-enable-bold . t))
+  (doom-themes-enable-bold . t)
+  (doom-challenger-deep-padded-modeline . t)
+  (doom-challenger-deep-comment-bg . t)
+  (doom-challenger-deep-brighter-comments . t)
+  (doom-challenger-deep-brighter-modeline . t))
+
+
 ;; (leaf srcery-theme
 ;;   :ensure t
 ;;   :config
@@ -183,99 +201,140 @@
            (company-selection-wrap-around . t))
   :global-minor-mode global-company-mode)
 
-(leaf ivy
-  :doc "Incremental Vertical completYon"
-  :req "emacs-24.5"
-  :tag "matching" "emacs>=24.5"
-  :url "https://github.com/abo-abo/swiper"
-  :emacs>= 24.5
+
+;; Emacsの次世代ミニバッファ補完UI
+;; https://blog.tomoya.dev/posts/a-new-wave-has-arrived-at-emacs/
+(leaf orderless
+  :doc "補完スタイルの提供"
   :ensure t
-  :blackout t
-  :leaf-defer nil
-  :custom
-  ((ivy-initial-inputs-alist . nil)
-   (ivy-re-builders-alist . '((t . ivy--regex-fuzzy)
-                              (swiper . ivy--regex-plus)
-                              (counsel-ag . ivy--regex-plus)
-                              (counsel-rg . ivy--regex-plus)))
-   (ivy-use-selectable-prompt . t)
-   (ivy-height . 30))
-  :global-minor-mode t
+  :custom (completion-styles . '(orderless)))
+(leaf marginalia
+  :ensure t
+  :init (marginalia-mode))
+(leaf vertico
+  :doc "ミニバッファ補完UI"
+  :ensure t
+  :global-minor-mode vertico-mode
+  :custom ((vertico-count . 20)))
+(leaf consult
+  :doc "補完候補リストの作成と便利な補完コマンド"
+  :url "https://github.com/minad/consult"
+  :ensure t
+  :bind (("C-x C-b" . consult-buffer)
+         ("C-x l" . consult-goto-line)
+         ("C-s" . consult-line)
+         ("C-c s" . consult-ripgrep))
+  ;; :custom ((consult-find-command . "fd --color=never --full-path ARG OPTS"))
   :config
-  (leaf swiper
-    :doc "Isearch with an overview. Oh, man!"
-    :req "emacs-24.5" "ivy-0.13.0"
-    :tag "matching" "emacs>=24.5"
-    :url "https://github.com/abo-abo/swiper"
-    :emacs>= 24.5
+  (leaf consult-ghq
+    :doc "ghq interface"
+    :if (executable-find "ghq")
     :ensure t
-    :bind (("C-s" . swiper)))
-
-  (leaf ag
-    :doc "Ag.el allows you to search using ag from inside Emacs. You can filter by file type, edit results inline, or find files."
+    :bind (("C-x g" . consult-ghq-find))
+    :custom ((consult-ghq-function . 'consult-find)))
+  (leaf consult-flycheck
     :ensure t)
-
-  (leaf counsel
-    :doc "Various completion functions using Ivy"
-    :req "emacs-24.5" "swiper-0.13.0"
-    :tag "tools" "matching" "convenience" "emacs>=24.5"
-    :url "https://github.com/abo-abo/swiper"
-    :emacs>= 24.5
-    :ensure t
-    :blackout t
-    :config
-    (defun my-counsel-ag ()
-      (interactive)
-      (counsel-ag "" default-directory))
-    :bind (("C-S-s" . counsel-imenu)
-           ("C-c s" . my-counsel-ag)
-           ("C-x C-r" . counsel-recentf)
-           ("C-x C-b" . counsel-switch-buffer))
-    :custom `((counsel-yank-pop-separator . "\n----------\n")
-              (counsel-find-file-ignore-regexp . ,(rx-to-string '(or "./" "../") 'no-group)))
-    :global-minor-mode t))
-
-(leaf ivy-rich
-  :doc "More friendly display transformer for ivy."
-  :req "emacs-24.5" "ivy-0.8.0"
-  :tag "ivy" "emacs>=24.5"
-  :emacs>= 24.5
+  (leaf consult-lsp
+    :ensure t)
+  (leaf embark-consult
+    :ensure t))
+(leaf embark
   :ensure t
-  :after ivy
-  :global-minor-mode t)
+  :bind ("M-s" . embark-act))
 
-(leaf ivy-ghq
-  :if (executable-find "ghq")
-  :el-get (ivy-ghq
-           :type github
-           :pkgname "analyticd/ivy-ghq")
-  :bind (("C-x g" . ivy-ghq-open))
-  :custom
-  (ivy-ghq-short-list . t))
+;; (leaf ivy
+;;   :doc "Incremental Vertical completYon"
+;;   :req "emacs-24.5"
+;;   :tag "matching" "emacs>=24.5"
+;;   :url "https://github.com/abo-abo/swiper"
+;;   :emacs>= 24.5
+;;   :ensure t
+;;   :blackout t
+;;   :leaf-defer nil
+;;   :custom
+;;   ((ivy-initial-inputs-alist . nil)
+;;    (ivy-re-builders-alist . '((t . ivy--regex-fuzzy)
+;;                               (swiper . ivy--regex-plus)
+;;                               (counsel-ag . ivy--regex-plus)
+;;                               (counsel-rg . ivy--regex-plus)))
+;;    (ivy-use-selectable-prompt . t)
+;;    (ivy-height . 30))
+;;   :global-minor-mode t
+;;   :config
+;;   (leaf swiper
+;;     :doc "Isearch with an overview. Oh, man!"
+;;     :req "emacs-24.5" "ivy-0.13.0"
+;;     :tag "matching" "emacs>=24.5"
+;;     :url "https://github.com/abo-abo/swiper"
+;;     :emacs>= 24.5
+;;     :ensure t
+;;     :bind (("C-s" . swiper)))
+
+;;   (leaf ag
+;;     :doc "Ag.el allows you to search using ag from inside Emacs. You can filter by file type, edit results inline, or find files."
+;;     :ensure t)
+
+;;   (leaf counsel
+;;     :doc "Various completion functions using Ivy"
+;;     :req "emacs-24.5" "swiper-0.13.0"
+;;     :tag "tools" "matching" "convenience" "emacs>=24.5"
+;;     :url "https://github.com/abo-abo/swiper"
+;;     :emacs>= 24.5
+;;     :ensure t
+;;     :blackout t
+;;     :config
+;;     (defun my-counsel-ag ()
+;;       (interactive)
+;;       (counsel-ag "" default-directory))
+;;     :bind (("C-S-s" . counsel-imenu)
+;;            ("C-c s" . my-counsel-ag)
+;;            ("C-x C-r" . counsel-recentf)
+;;            ("C-x C-b" . counsel-switch-buffer))
+;;     :custom `((counsel-yank-pop-separator . "\n----------\n")
+;;               (counsel-find-file-ignore-regexp . ,(rx-to-string '(or "./" "../") 'no-group)))
+;;     :global-minor-mode t))
+
+;; (leaf ivy-rich
+;;   :doc "More friendly display transformer for ivy."
+;;   :req "emacs-24.5" "ivy-0.8.0"
+;;   :tag "ivy" "emacs>=24.5"
+;;   :emacs>= 24.5
+;;   :ensure t
+;;   :after ivy
+;;   :global-minor-mode t)
+
+;; (leaf ivy-ghq
+;;   :if (executable-find "ghq")
+;;   :el-get (ivy-ghq
+;;            :type github
+;;            :pkgname "analyticd/ivy-ghq")
+;;   :bind (("C-x g" . ivy-ghq-open))
+;;   :custom
+;;   (ivy-ghq-short-list . t))
 
 
-(leaf prescient
-  :doc "Better sorting and filtering"
-  :req "emacs-25.1"
-  :tag "extensions" "emacs>=25.1"
-  :url "https://github.com/raxod502/prescient.el"
-  :emacs>= 25.1
-  :ensure t
-  :commands (prescient-persist-mode)
-  :custom `((prescient-aggressive-file-save . t)
-            (prescient-save-file . ,(locate-user-emacs-file "prescient")))
-  :global-minor-mode prescient-persist-mode)
+;; (leaf prescient
+;;   :doc "Better sorting and filtering"
+;;   :req "emacs-25.1"
+;;   :tag "extensions" "emacs>=25.1"
+;;   :url "https://github.com/raxod502/prescient.el"
+;;   :emacs>= 25.1
+;;   :ensure t
+;;   :commands (prescient-persist-mode)
+;;   :custom `((prescient-aggressive-file-save . t)
+;;             (prescient-save-file . ,(locate-user-emacs-file "prescient")))
+;;   :global-minor-mode prescient-persist-mode)
 
-(leaf ivy-prescient
-  :doc "prescient.el + Ivy"
-  :req "emacs-25.1" "prescient-4.0" "ivy-0.11.0"
-  :tag "extensions" "emacs>=25.1"
-  :url "https://github.com/raxod502/prescient.el"
-  :emacs>= 25.1
-  :ensure t
-  :after prescient ivy
-  :custom ((ivy-prescient-retain-classic-highlighting . t))
-  :global-minor-mode t)
+;; (leaf ivy-prescient
+;;   :doc "prescient.el + Ivy"
+;;   :req "emacs-25.1" "prescient-4.0" "ivy-0.11.0"
+;;   :tag "extensions" "emacs>=25.1"
+;;   :url "https://github.com/raxod502/prescient.el"
+;;   :emacs>= 25.1
+;;   :ensure t
+;;   :after prescient ivy
+;;   :custom ((ivy-prescient-retain-classic-highlighting . t))
+;;   :global-minor-mode t)
 
 (leaf anzu
   :doc "provides a minor mode which displays current match and total matches information in the mode-line in various search modes"
@@ -364,11 +423,11 @@
   :init (yas-global-mode)
   :hook ((rustic-mode . lsp-deferred)
          (ruby-mode-hook . lsp-deferred)
-         (tsx-mode-hook . lsp-deferred))
+         (tsx-mode-hook . lsp-deferred)
+         (conf-toml-mode-hook . lsp-deferred)) ;; require: `cargo install taplo-cli --features lsp`
   :bind
-  (lsp-mode-map ("C-c h" . lsp-describe-thing-at-point)
+  (("C-c h" . lsp-describe-thing-at-point)
    ("C-c C-c a" . lsp-execute-code-action)
-   ("C-c C-c e" . lsp-rust-analyzer-expand-macro)
    ("C-c C-c r" . lsp-rename)
    ("M-." . lsp-find-definition))
   :custom
@@ -377,6 +436,7 @@
    (lsp-restart . 'auto-restart)
    (lsp-log-io . nil)
    (lsp-eldoc-render-all . t)
+   (lsp-lens-mode . t)
    ;; (lsp-idle-delay . 0.1)
    ;; ruby
    (lsp-solargraph-use-bundler . t)
@@ -389,13 +449,13 @@
    (lsp-rust-analyzer-server-display-inlay-hints . t)
    ;; typescript/js
    (lsp-eslint-enable . t) ;; requires run lsp-install-server
-   (lsp-eslint-autofix-on-save . t))
+   (lsp-eslint-autofix-on-save . t) ;; does not worked https://github.com/emacs-lsp/lsp-mode/issues/1842
+   )
   :config
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
 (leaf lsp-ui
   :ensure t
-  :after lsp-deffered
   :commands lsp-ui-mode
   :custom
   (lsp-ui-doc-border . (face-foreground 'default))
@@ -406,8 +466,9 @@
   (lsp-ui-doc-max-width . 150)
   (lsp-ui-doc-max-height . 30)
   (lsp-ui-doc-position . 'at-point)
-  (lsp-ui-doc-use-childframe . nil)
+  (lsp-ui-doc-use-childframe . t)
   (lsp-ui-doc-use-webkit . nil)
+  (lsp-ui-doc-show-with-cursor . t)
   (lsp-ui-peek-always-show . t)
   (lsp-ui-peek-enable . t)
   (lsp-ui-peek-peek-height . 20)
@@ -416,6 +477,7 @@
   (lsp-ui-sideline-delay . 0.05)
   (lsp-ui-sideline-show-hover . t)
   (lsp-ui-sideline-show-code-actions . t))
+
 (leaf magit
   :ensure t)
 
@@ -482,6 +544,10 @@
   :ensure t
   :defvar (flycheck-checkers)
   :defun (rust-format-region)
+  :bind
+  (rustic-mode-map
+   ("C-c C-c e" . lsp-rust-analyzer-expand-macro)
+   ("C-c C-c t" . rustic-cargo-test))
   :custom ((rustic-format-on-save . t))
   :config
   (push 'rustic-clippy flycheck-checkers))
@@ -520,11 +586,18 @@
   (leaf *tsi :el-get "orzechowskid/tsi.el"))
 (leaf origami :ensure t)
 (leaf coverlay :ensure t)
-
+(leaf graphql-mode :ensure t)
 (leaf tsx-mode
   :require origami tree-sitter tree-sitter-langs tsi coverlay
   :el-get "orzechowskid/tsx-mode.el"
-  :mode "\\(\.ts\\|\.tsx\\)")
+  :bind
+  (tsx-mode-map
+   ("C-c C-c f" . lsp-eslint-apply-all-fixes))
+  :mode
+  (("\\.ts$" . tsx-mode)
+   ("\\.tsx$" . tsx-mode))
+  :config
+  (add-hook 'before-save-hook 'lsp-eslint-apply-all-fixes))
 
 (provide 'init)
 
